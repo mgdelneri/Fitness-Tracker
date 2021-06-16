d0c9ed6d859faeb995474e6aac6f4a58ab341fdf
Marina <mgdelneri@gmail.com>
2021-06-16 13:33:21 -0500

    Installed node modules; Started server.js file

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3000;
+
+const User = require("./userModel.js");
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONOGDB_URI || "mongodb://localhost/userdb", { useNewUrlParser: true });
+
+// TODO: routes
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+});
