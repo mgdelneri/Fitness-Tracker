410c6b7c85f22962dc606ce17094e57495cfe6fc
Marina <mgdelneri@gmail.com>
2021-06-18 16:05:51 -0500

    Finished coding server.js; Added models folder.

--- server.js
+++ server.js
@@ -4,19 +4,19 @@ const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
 
-const db = require("./models.js");
 const app = express();
 
 app.use(logger("dev"));
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static("public"));
 
 mongoose.connect(process.env.MONOGDB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 
-// TODO: routes
+const db = require("./models");
+
+require("./routes/apiRoutes")(app);
+require("./routes.htmlRoutes")(app);
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
