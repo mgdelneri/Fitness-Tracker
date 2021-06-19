1f091933b84a496715610b4b87497b9c7fbb1dac
Marina <mgdelneri@gmail.com>
2021-06-19 13:22:41 -0500

    Edited htmlRoutes.js

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,18 +1,20 @@
+const router = require("express").Router();
 const path = require("path");
 
-module.exports = function(app) {
-    // GET index.html page
-    app.get("/", function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/index.html"));
-    });
+// GET index.html page
+router.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+});
 
-    // GET when either "continue workout" or "new workout" is chosen
-    app.get("/exercise", function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/exercise.html"));
-    });
+// GET when either "continue workout" or "new workout" is chosen
+router.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
+});
 
-    // GET stats page when dashboard is chosen
-    app.get("/stats", function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/stats.html"));
-    })
-}
+// GET stats page when dashboard is chosen
+router.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
+});
+
+module.exports = router; 
+
